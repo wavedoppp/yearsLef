e8eb79fb5010b974b6a1085e5de3b037e649fde3
wavedoppp <21032003k@gmail.com>
2023-08-24 15:18:56 +0300

    Добавьте файлы проекта.

---
+++ APP4/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace LifeCountdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите Имя: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Введите Фамилию: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Введите ваш возраст: ");
+            int age = int.Parse(Console.ReadLine());
+
+            
+            int yearsLeft = 40 - age;
+
+            
+            string outputText = $"Привет, {firstName} {surname}! Тебе осталось {yearsLeft} лет до сорока лет!";
+
+            
+            Console.WriteLine(outputText);
+            Console.ReadLine();
+            return;
+        }
+    }
+}
